822d9b4506740dbb26689677a6bbea39cfa0a0ae
Chris Mochinski <cmochinski@gmail.com>
2022-01-16 15:54:01 -0600

    children proptypes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+type NameTagProps = {
+  name: string;
+};
 
-function App() {
+const NameTag = (props: NameTagProps) => {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main>
+      <header>
+        <h1>Hello</h1>
+        <p>My Name Is</p>
       </header>
-    </div>
+      <section className="display-name">
+        <p>{props.name}</p>
+      </section>
+      <footer />
+    </main>
   );
-}
+};
+
+const App = () => <NameTag name="Mo" />;
 
 export default App;
